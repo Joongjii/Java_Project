09c85732f18e102bf94e121800b95f79336eaca0
[Joongjii] <[guswnddl1217@gmail.com]>
2023-01-16 21:28:23 +0900

    QRServer

---
+++ QRServer/src/com/mc/qrserver/Run.java
@@ -0,0 +1,12 @@
+package com.mc.qrserver;
+
+import com.mc.qrserver.presentaion.QRContorller;
+
+public class Run {
+
+	public static void main(String[] args) {
+		new QRContorller().startServer();
+		
+	}
+
+}
